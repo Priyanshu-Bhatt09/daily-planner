be546c144b41107a2c3ad8a84be4caf9c7f4b391
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-02-17 15:24:01 +0530

    added frontend link from vercel

--- DailySpring-backend/src/main/java/com/example/demo/controller/PlanController.java
+++ DailySpring-backend/src/main/java/com/example/demo/controller/PlanController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/daily")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://momentum-eosin.vercel.app")
 public class PlanController {
     private final PlanService service;
     public PlanController(PlanService service) {
